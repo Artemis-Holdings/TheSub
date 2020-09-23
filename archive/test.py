1fc7ea8249cf40f13c0278040742d835dd23436d
Justin Peter <justinhasanaccount@gmail.com>
2020-09-23 14:09:21 -0400

    Improved network address calculations. Added comments.

--- archive/test.py
+++ archive/test.py
@@ -3,7 +3,7 @@ db = {0: {'net_common_name': 'sadfdsf', 'hosts_req': 99, 'hosts_avail': 126, 'ho
           'sub_end': [192, 168, 0, 126], 'sub_broad': [192, 168, 1, 127], 'sub_wild': [0, 0, 0, 127],
           'sub_delta_r': 128},
       1: {'net_common_name': 'assdaas', 'hosts_req': 66, 'hosts_avail': 126, 'hosts_unused': 60,
-          'net_add': [192, 168, 0, 128], 'cidr': 25, 'sub_mask': [255, 255, 255, 128], 'sub_start': [192, 168, 0, 129],
+          'net_add': [0, 0, 0, 0], 'cidr': 25, 'sub_mask': [255, 255, 255, 128], 'sub_start': [0, 0, 0, 0],
           'sub_end': [192, 168, 0, 254], 'sub_broad': [192, 168, 0, 255], 'sub_wild': [0, 0, 0, 127],
           'sub_delta_r': 128},
       2: {'net_common_name': 'srag', 'hosts_req': 33, 'hosts_avail': 62, 'hosts_unused': 29,
@@ -12,19 +12,33 @@ db = {0: {'net_common_name': 'sadfdsf', 'hosts_req': 99, 'hosts_avail': 126, 'ho
           'sub_broad': [192, 168, 0, 319], 'sub_wild': [0, 0, 0, 63], 'sub_delta_r': 64}}
 user_db = [3, [192, 168, 1, 1], [255, 255, 255, 0], 24]
 
+# net_add from DB 'sub_broad': [192, 168, 1, 127]
+def find_net(db, net_add):
+    if net_add[3] <= 255:
+        db['net_add'][3] = net_add[3] + 1
+        db['net_add'][2] = net_add[2]
+        db['net_add'][1] = net_add[1]
+        db['net_add'][0] = net_add[0]
+    elif net_add[2] <= 255:
+        db['net_add'][3] = int(0)
+        db['net_add'][2] = net_add[2] + 1
+        db['net_add'][1] = net_add[1]
+        db['net_add'][0] = net_add[0]
+    elif net_add[1] <= 255:
+        db['net_add'][3] = int(0)
+        db['net_add'][2] = int(0)
+        db['net_add'][1] = net_add[1] + 1
+        db['net_add'][0] = net_add[0]
+    else:
+        db['net_add'][3] = int(0)
+        db['net_add'][2] = int(0)
+        db['net_add'][1] = int(0)
+        db['net_add'][0] = net_add[1] + 1
 
-def printer_lite(db, input_user_db):
-    table = []
-    for header in db[0].keys():
-        print(header, end=',')
-    for i in range(0, input_user_db[0]):
-        table.append(db[i].values())
-        x = table[i]
-        y = list(x)
-        for j in range(0, len(y)):
-            d = y[j]
-            print(d, end=',')
+    start = db['net_add'].copy()
+    return start
 
 
-
-printer_lite(db, user_db)
+# index 1 should replace by i during production
+#db at i (first position) assigns the dictionary
+print(find_net(db[1], db[1 - 1].get('sub_broad')))
--- main.py
+++ main.py
@@ -9,28 +9,28 @@
 # The Sub: Expeditionary VLSM Calculator
 # Documentation at: https://github.com/TheMagicNacho
 # v0.1.1
-#TODO: Add Max available subnet validator
+# TODO: Add Max available subnet validator
 # ------------------------------------------------
 from tabulate import tabulate
 
 
-def main():
+def main():  # Main function loops through user input, calculations, and input validation.
     try:
-        usr_input_network = init_input()
-        input_user_db, input_labels_db = mask_input(usr_input_network)
+        usr_input_network = init_input()  # Gather user input
+        input_user_db, input_labels_db = mask_input(usr_input_network)  # Build the databases
         db = vlsm(input_user_db, input_labels_db)
-        try:
+        try:  # Pints fully formatted calculations.
             printer(db, input_user_db)
             printer_lite(db, input_user_db)
-        except:
+        except:  # Prints only CSV because tabulate module is missing.
             print('!!!Tabulate module missing. Use CSV!!!')
             printer_lite(db, input_user_db)
-    except:
+    except:  # Restarts in the event of impassable error.
         print('RESTART: General Error.')
         main()
 
 
-def vlsm(input_user_db, input_labels_db):
+def vlsm(input_user_db, input_labels_db):  # Primary manager for calculating operations
     og = dict(net_common_name='name', hosts_req=0, hosts_avail=0, hosts_unused=0, net_add=[0, 0, 0, 0], cidr=0,
               sub_mask=[0, 0, 0, 0], sub_start=[0, 0, 0, 0], sub_end=[0, 0, 0, 0], sub_broad=[0, 0, 0, 0],
               sub_wild=[0, 0, 0, 0], sub_delta_r=0)
@@ -40,9 +40,9 @@ def vlsm(input_user_db, input_labels_db):
         db[i] = og.copy()
         db[i]['net_common_name'] = input_labels_db[i][0]
         db[i]['hosts_req'] = input_labels_db[i][1]
-        if i == 0:
-            db[0]['net_add'] = input_user_db[1].copy()
-            db[0]['cidr'] = int(find_slash(db[0].get('hosts_req')))
+        if i == 0:  # Establish first subnet
+            db[0]['net_add'] = input_user_db[1].copy()  # Initial net address comes from user input.
+            db[0]['cidr'] = int(find_slash(db[0].get('hosts_req')))  #  Find the CIDR if dotted decimal is inputed, convert to CIDR
             db[0]['hosts_avail'] = int(find_hosts(db[0].get('cidr')))
             db[0]['hosts_unused'] = db[i].get('hosts_avail') - db[0].get('hosts_req')
             db[0]['sub_mask'] = find_mask(db[0].get('cidr'))
@@ -53,12 +53,12 @@ def vlsm(input_user_db, input_labels_db):
             db[0]['sub_end'] = find_end(db[0], db[0].get('sub_broad'))
 
 
-        else:
-            db[i]['net_add'] = find_start(db[i], db[i - 1].get('sub_broad'))
-            db[i]['cidr'] = int(find_slash(db[i].get('hosts_req')))
+        else:  # Operate upon subsequent sub-nets
+            db[i]['cidr'] = int(find_slash(db[i].get('hosts_req')))  # Input CIDR from previous calculations
+            db[i]['sub_mask'] = find_mask(db[i].get('cidr'))
+            db[i]['net_add'] = find_net(db[i], db[i - 1].get('sub_broad'))
             db[i]['hosts_avail'] = int(find_hosts(db[i].get('cidr')))
             db[i]['hosts_unused'] = db[i].get('hosts_avail') - db[i].get('hosts_req')
-            db[i]['sub_mask'] = find_mask(db[i].get('cidr'))
             db[i]['sub_delta_r'] = db[i].get('hosts_avail') + 2
             db[i]['sub_start'] = find_start(db[i], db[i].get('net_add'))
             db[i]['sub_wild'] = find_wildcard(db[i].get('sub_mask'))
@@ -67,10 +67,28 @@ def vlsm(input_user_db, input_labels_db):
     return db
 
 
-def find_start(db, net_add):
-    db['sub_start'][3] = net_add[3] + 1
-    for b in range(0, 2):
-        db['sub_start'][b] = net_add[b]
+def find_start(db, net_add):  # Operates similarly to the find_net. Input: network Address / Output: Start
+    if net_add[3] <= 254:
+        db['sub_start'][3] = net_add[3] + 1
+        db['sub_start'][2] = net_add[2]
+        db['sub_start'][1] = net_add[1]
+        db['sub_start'][0] = net_add[0]
+    elif net_add[2] <= 254:
+        db['sub_start'][3] = int(0)
+        db['sub_start'][2] = net_add[2] + 1
+        db['sub_start'][1] = net_add[1]
+        db['sub_start'][0] = net_add[0]
+    elif net_add[1] <= 254:
+        db['sub_start'][3] = int(0)
+        db['sub_start'][2] = int(0)
+        db['sub_start'][1] = net_add[1] + 1
+        db['sub_start'][0] = net_add[0]
+    else:
+        db['sub_start'][3] = int(0)
+        db['sub_start'][2] = int(0)
+        db['sub_start'][1] = int(0)
+        db['sub_start'][0] = net_add[1] + 1
+
     start = db['sub_start'].copy()
     return start
 
@@ -83,10 +101,10 @@ def find_end(db, net_broad):
     return end
 
 
-def find_slash(sn_hosts):
+def find_slash(sn_hosts):  # Used to find the CIDR for subnets past the initial subnet.
     for i in range(0, sn_hosts):
         x = 2 ** i
-        if x > sn_hosts: #default + 2
+        if x > sn_hosts + 2:
             return 32 - i
         else:
             continue
@@ -139,7 +157,7 @@ def return_cidr_normalized(input_cidr):
         main()
 
 
-def dec_to_bin(mask_array):
+def dec_to_bin(mask_array): # Create binary array from function input
     mask_bin = []
     for i in range(0, 4):
         z = str(bin(mask_array[i]).replace("0b", ""))
@@ -153,7 +171,7 @@ def find_hosts(found_cidr):
     return (2 ** x) - 2
 
 
-def find_mask(c):
+def find_mask(c):  # Create the sub-net mask by using the CIDR. Only called if CIDR is provided by user.
     array_mask = []
     for i in range(0, 4):
         array_mask.append(i)
@@ -187,40 +205,65 @@ def find_mask(c):
                 array_mask[3] = 256 - (2 ** z)
     return array_mask
 
+## Hidden function. This works, but is not very reliable. Using a more robust function title: find_net / Keeping for possible usage in the future.
+# def find_net_add(net, mask):
+#     c = [1, 1, 1, 1]  # Temporary, colapsable database
+#     for i in range(0, 4):
+#         c[i] = net[i] & mask[i]  # AND the network and the mask
+#     return c
 
-def find_net_add(net, mask):
-    c = [1, 1, 1, 1]
-    for i in range(0, 4):
-        c[i] = net[i] & mask[i]
-    return c
+
+def find_net(db, net_add):  # Outputs the network address. Input: Broadcast Process: If within range add 1 to octett. Output: Updated network.
+    if net_add[3] <= 254:
+        db['net_add'][3] = net_add[3] + 1
+        db['net_add'][2] = net_add[2]
+        db['net_add'][1] = net_add[1]
+        db['net_add'][0] = net_add[0]
+    elif net_add[2] <= 254:
+        db['net_add'][3] = int(0)
+        db['net_add'][2] = net_add[2] + 1
+        db['net_add'][1] = net_add[1]
+        db['net_add'][0] = net_add[0]
+    elif net_add[1] <= 254:
+        db['net_add'][3] = int(0)
+        db['net_add'][2] = int(0)
+        db['net_add'][1] = net_add[1] + 1
+        db['net_add'][0] = net_add[0]
+    else:
+        db['net_add'][3] = int(0)
+        db['net_add'][2] = int(0)
+        db['net_add'][1] = int(0)
+        db['net_add'][0] = net_add[1] + 1
+
+    start = db['net_add'].copy()
+    return start
 
 
 def find_wildcard(sub_mask):
-    c = [1, 1, 1, 1]
+    c = [1, 1, 1, 1]  # Temporary, colapsable database
     for b in range(0, 4):
         c[b] = 255 - sub_mask[b]
     return c
 
 
 def find_broadcast(wildcard, net_add):
-    d = [1, 1, 1, 1]
+    d = [1, 1, 1, 1]  # Temporary, colapsable database
     for i in range(0, 4):
-        d[i] = wildcard[i] | int(net_add[i])
+        d[i] = wildcard[i] | int(net_add[i]) #  OR operation
     return d
 
 
-def printer(db, u):
+def printer(db, u):  # Pretty print the the information.
     table = []
     print('EASY READ FORMAT')
-    print("==================")
+    print("==================")  # I thought about using code to write the divider, but decided that was a waste of lines.
     for i in range(0, u[0]):
         table.append(db[i].values())
     headers = db[0].keys()
     print(tabulate(table, headers, tablefmt="pipe", stralign='center', numalign='left'))
 
 
-
-def printer_lite(db, input_user_db):
+def printer_lite(db, input_user_db):  # Create the CSV format of information.
     table = []
     print('CSV FORMAT\n~~Copy and Paste the following into a .txt then import the file to excel. Delimitator is colon '
           '(:)~~')
@@ -239,7 +282,7 @@ def printer_lite(db, input_user_db):
     main()
 
 
-def validate_ip(ip):
+def validate_ip(ip):  # Validate IP. If not valid IP, return feedback then restart the program.
     ip = ip.split('.')
     try:
         for i in range(0, 4):
@@ -253,7 +296,6 @@ def validate_ip(ip):
                     pass
                 else:
                     print('RESTART: IP address too short.')
-
                     main()
             else:
                 print('RESTART: Error with octett {}'.format(ip[i]))
@@ -263,7 +305,7 @@ def validate_ip(ip):
         main()
 
 
-def pre_process(input_network, input_mask, n_nets):
+def pre_process(input_network, input_mask, n_nets):  # Apply required codecs, then write fundamental database.
     input_user_db = []
 
     net_mask_array = return_mask_normalized(input_mask)
@@ -275,7 +317,7 @@ def pre_process(input_network, input_mask, n_nets):
     return input_user_db
 
 
-def init_input():
+def init_input():  # Recieve user input and validate
     input_network = input('What is the network IPv4 address: ')
     ip_code = validate_ip(input_network)
     if ip_code is None:
@@ -286,14 +328,14 @@ def init_input():
     return input_network
 
 
-def mask_input(input_net):
+def mask_input(input_net):  # Recieve user input for mask & Network data. Validate.
     input_labels_db = []
-
+#  Mask input
     input_mask = input('What is the mask (255.255.255.192) or CIDR (/24): ')
     n_nets = int(input('Number of subnets required: '))
     input_user_db = pre_process(input_net, input_mask, n_nets)
     for b in range(0, n_nets):
-
+#  Network input
         name_sub_net = input('Network ' + str(b) + ' Common Name: ')
         try:
             n_hosts = int(input('Number of hosts required: '))
@@ -301,11 +343,9 @@ def mask_input(input_net):
             input_labels_db.append(new_entry)
             input_labels_db = sorted(input_labels_db, key=lambda x: x[1], reverse=True)
         except:
-            print('IGNORING INPUT: Value must be an integer.')
+            print('IGNORING INPUT: Value must be an integer.')  # Data validation is go/no-go for integer
             pass
     return input_user_db, input_labels_db
-
-
 
 
 if __name__ == '__main__':
